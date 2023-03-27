de6dd3390a6afe4ee386cf1d1c2925b1b2a9cb35
Sarah Cervantes <blissophist@gmail.com>
2023-03-26 23:39:18 -0400

    main

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://howtoreadacrochetpattern_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "How to Read Crochet Patterns"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;By the end of this standalone module, you will be able to read any pattern with confidence and be able to crochet the items you wish to. &lt;/p&gt;"
+};
